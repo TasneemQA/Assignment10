7491631794ca9080ebe7dc5b6d9982f7ffff2041
tasneem qanoa <simora2017@gmail.com>
2024-12-30 15:01:02 +0200

    new edit

--- Assets/Scripts/Assignment29/ExceptionHandling.cs
+++ Assets/Scripts/Assignment29/ExceptionHandling.cs
@@ -3,27 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-// 1. Set up a Score Calculation Scenario:
-// ○ Declare two int variables:
-// ■ playerScore = 100.
-// ■ diviser = 0 (intentionally set to 0 to simulate division by zero).
-// 2. Add a Try-Catch-Finally Block:
-// ○ Try Block:
-// ■ Attempt to calculate the player's total score by dividing playerScore by
-// diviser.
-// ■ Use the formula: int totalScore = playerScore / diviser;
-// ■ Log "Attempting to calculate score multiplier...".
-// ○ Catch Block:
-// ■ Catch the DivideByZeroException.
-// ■ Log an error message: "Error: Division by zero occurred
-// while calculating score multiplier.".
-// ○ Finally Block:
-// ■ Log "Score calculation complete. Cleaning up
-// resources.".
-// Expected Unity Console Output
-// Attempting to calculate score multiplier...
-// Error: Division by zero occurred while calculating score multiplier.
-// Score calculation complete. Cleaning up resources.
 public class ExceptionHandling : MonoBehaviour
 {
     // Start is called before the first frame update
